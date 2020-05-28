11fbc07f6fc687bb3a29af3eaabb41135d5c5497
Jim Clark <slimslenderslacks@gmail.com>
2020-05-27 17:03:37 -0700

    Bump github-repo-config-skill to 0.1.4 and spit metadata

--- src/atomist/skill_meta.cljs
+++ src/atomist/skill_meta.cljs
@@ -3,6 +3,6 @@
 
 (def module-name "github-repo-config-skill")
 (def path ".")
-(def version "0.1.3")
-(def tag "0.1.3")
-(def generated-at "2020-05-27T23:35:49Z")
+(def version "0.1.4")
+(def tag "0.1.4")
+(def generated-at "2020-05-28T00:03:37Z")
